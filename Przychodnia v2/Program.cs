aecf5c1d2c90882db9bb3ea5f6bb6585f5518334
DamianJayblonski <damian.jablonski@microsoft.wsei.edu.pl>
2022-05-22 13:46:01 +0200

    Wprowadzanie danych do tabeli

--- Przychodnia v2/Program.cs
+++ Przychodnia v2/Program.cs
@@ -7,10 +7,48 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            using(var databaseDJ = new PacjentContext())
+
+
+            using (var databaseDJ = new PacjentContext())
             {
-                databaseDJ.Add(new Pacjent() { Name = "KOWALSKI" });
+                /*
+                databaseDJ.Add(new Pacjent() { 
+                    Nazwisko = "Kowalski", 
+                    Imie = "Antoni", 
+                    Adres = "ul. Błotna numer zachloapany Krakow woj. Mał",
+                    DataUrodzenia = new DateTime(1976, 1, 18, 0, 0, 0),
+                    Plec = "M",
+                    Pesel = "76011812222"
+                });
+               
+
+                databaseDJ.Add(new RodzajZabiegu()
+                {
+                    Nazwa = "Laser",
+                    Numer = "01-LSR",
+                });
+
+                databaseDJ.Add(new RodzajZabiegu()
+                {
+                    Nazwa = "Masaż",
+                    Numer = "02-M45",
+                });
+
+                databaseDJ.Add(new RodzajZabiegu()
+                {
+                    Nazwa = "Akupunktura",
+                    Numer = "03-APK",
+                });
+
+                databaseDJ.Add(new RodzajZabiegu()
+                {
+                    Nazwa = "Ostre Ciecie",
+                    Numer = "04-OCC",
+                });
+
                 databaseDJ.SaveChanges();
+
+                */
             }
 
             Console.WriteLine("Finished");
