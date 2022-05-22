c78203e45530ee854d7d523e0d2c86c8340f94e3
DamianJayblonski <damian.jablonski@microsoft.wsei.edu.pl>
2022-05-22 13:09:27 +0200

    Pierwsze tabele i relacje v1

--- Przychodnia v2/Pacjent.cs
+++ Przychodnia v2/Pacjent.cs
@@ -10,14 +10,17 @@ namespace Przychodnia_v2
     public class PacjentContext : DbContext
     {
         public DbSet<Pacjent> Pacjents { get; set; }
+        public DbSet<Rozpoznanie> Rozpoznanies { get; set; }
+        public DbSet<Wypis> Wypiss { get; set; }
+        public DbSet<RodzajZabiegu> RodzajZabiegus { get; set; }
 
         public string DbPath { get; }
-        
-        public PacjentContext() 
+
+        public PacjentContext()
         {
             var folder = Environment.SpecialFolder.LocalApplicationData;
             var path = Environment.GetFolderPath(folder);
-            DbPath = System.IO.Path.Join(path, "pacjent05.db");
+            DbPath = System.IO.Path.Join(path, "Przychodnia\\Przychodnia01.db");
 
         }
 
@@ -29,19 +32,42 @@ namespace Przychodnia_v2
         }
 
     }
-    public class Pacjent 
+    public class Pacjent
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public DateTime DataUrodzenia { get; set; }
+        public ICollection<Rozpoznanie> Rozpoznanies { get; set; }
+        public ICollection<Wypis> Wypiss { get; set; }
 
-        public string Name { get; set; }
-
-
-
-
-
-
-
-
+    }
+    public class Rozpoznanie
+    {
+        public int ID { get; set; }
+        public string Nazwa { get; set; }
+        public DateTime Data { get; set; }
+        public int RodzajZabieguID { get; set; }
+        public RodzajZabiegu RodzajZabiegu { get; set; }
+        public int PacjentID { get; set; }
+        public Pacjent Pacjent { get; set; }
+    }
+    public class Wypis
+    {
+        public int ID { get; set; }
+        public string Rezultat { get; set; }
+        public DateTime DataWypisu { get; set; }
+        public DateTime DataLeczeniaOd { get; set; }
+        public DateTime DataLeczeniaDo { get; set; }
+        public int PacjentID { get; set; }
+        public Pacjent Pacjent { get; set; }
+    }
+    public class RodzajZabiegu
+    {
+        public int ID { get; set; }
+        public string Nazwa { get; set; }
+        public string Numer { get; set; }
+        public ICollection<Rozpoznanie> Rozpoznanies { get; set; }
 
     }
 }
